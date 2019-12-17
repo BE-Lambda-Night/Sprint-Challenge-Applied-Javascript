60a4421d09c9c595af879a74ddb47bbc05260055
BryceEvans <evans.bryce@gmail.com>
2019-12-17 11:33:24 -0700

    getting this done quickly

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,48 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios
+  .get('https://lambda-times-backend.herokuapp.com/articles')
+  .then((res) => {
+    // console.log(res)
+    // console.log(res.data)
+    const theArticlesData = res.data.articles
+    console.log(res.data.articles)
+  })
+  .catch((err) => {
+    console.log("Error: ", err)
+  })
+
+function cardCreator(articleObject) {
+
+  // createElement
+  const card = document.createElement('div')
+  const headline = document.createElement('div')
+  const author = document.createElement('div')
+  const imgContain = document.createElement('div')
+  const image = document.createElement('img')
+  const authorSpan = document.createElement('span')
+
+  // add classNames
+  card.classList.add('card')
+  headline.classList.add('headline')
+  author.classList.add('author')
+  imgContain.classList.add('img-container')
+
+  // add textContent and img src
+  headline.textContent = article.headline
+  img.src = article.authorPhoto
+  authorSpan.textContent = "By " + article.authorName
+
+  // append
+  card.appendChild(headline)
+  card.appendChild(author)
+  author.appendChild(imgContain)
+  imgContain.appendChild(image)
+  author.appendChild(authorSpan)
+
+  // return
+  return card
+
+}
--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,33 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const mainHeader = document.querySelector('div.header-container')
+// console.log(mainHeader)
+mainHeader.appendChild(Header())
+
+function Header() {
+
+  // createElement
+  const header = document.createElement('div')
+  const span = document.createElement('span')
+  const header1 = document.createElement('h1')
+  const secondSpan = document.createElement('span')
+
+  // add classes
+  header.classList.add('header')
+  span.classList.add('date')
+  secondSpan.classList.add('temp')
+
+  // textContent
+  span.textContent = "SMARCH 28, 2019"
+  header1.textContent = "Lambda Times"
+  secondSpan.textContent = "98°"
+
+  // append
+  header.appendChild(span)
+  header.appendChild(header1)
+  header.appendChild(secondSpan)
+
+  // return header
+  return header
+}
